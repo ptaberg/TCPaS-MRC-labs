7779af8a2e869007e72c5073619f2f628f98369d
Kolja Ptaberg <ptashym@gmail.com>
2020-02-27 00:29:26 +0300

    fcfs complete algorithm

--- main.go
+++ main.go
@@ -25,23 +25,59 @@ type Process struct {
 	val []State
 }
 
+func (p *Process) appendToState (s State) []State {
+	p.val = append(p.val, s);
+	return p.val;
+}
+
+type filled struct {
+	end int
+}
+
+func (f *filled) canBeFilled(n int) bool {
+	a := n > f.end;
+	return a;
+}
 
 type Algo struct {};
 
 func (a *Algo) FCFS(tasks []Task) {
 	// sort by start INT moment
-
-	var prs []Process;
-
 	sort.SliceStable(tasks, func(i, j int) bool {
 		return tasks[i].start < tasks[j].start;
 	})
 
+	// iterate the processes
+	prs := make([]Process, len(tasks));
+	var lastEnd int = 0;
 	for i := 0; i < len(tasks); i++ {
+		prs[i].id = tasks[i].id;
+		var pEnd int = tasks[i].start + tasks[i].time;
+		var pStart int = tasks[i].start;
 
+		//iterate and fill processes
+		for j := 0; j < 20; j++ {
+			if i == 0 {
+				if j >= pStart && j <= pEnd {
+					prs[i].appendToState(p)
+				} else {
+					prs[i].appendToState(n)
+				}
+			} else {
+				if j >= pStart && j <= lastEnd {
+					prs[i].appendToState(r);
+					pEnd++;
+				} else if j > lastEnd && j <= pEnd {
+					prs[i].appendToState(p)
+				} else {
+					prs[i].appendToState(n)
+				}
+			}
+		}
+		lastEnd = pEnd;
 	}
 
-	fmt.Println(tasks, prs);
+	fmt.Println(prs);
 }
 
 func main() {
@@ -52,12 +88,12 @@ func main() {
 		{
 			id:1,
 			time:3,
-			start:2,
+			start:1,
 		},
 		{
 			id:2,
 			time:4,
-			start:0,
+			start:3,
 		},
 		{
 			id:3,
