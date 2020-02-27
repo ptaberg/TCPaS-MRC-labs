b7842d3de9bd1c2c97e78b996a17b7d8b7709921
Kolja Ptaberg <ptashym@gmail.com>
2020-02-27 15:49:57 +0300

    feat: server starter kit

---
+++ algorithms/fcfs.go
@@ -0,0 +1,10 @@
+package algorithms
+
+func fcfs(b int) int {
+	a := 3 + b
+	return a
+}
+
+func main() {
+	fcfs(3)
+}
--- main.go
+++ main.go
@@ -2,17 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"sort"
 )
 
 type Task struct {
-	id int
-	time int
-	start int
+	id       int
+	time     int
+	start    int
 	priority int
 }
 
-type State int;
+type State int
 
 const (
 	n State = 0
@@ -21,37 +23,37 @@ const (
 )
 
 type Process struct {
-	id int
+	id  int
 	val []State
 }
 
-func (p *Process) appendToState (s State) []State {
-	p.val = append(p.val, s);
-	return p.val;
+func (p *Process) appendToState(s State) []State {
+	p.val = append(p.val, s)
+	return p.val
 }
 
-type Algo struct {};
+type Algo struct{}
 
 func (a *Algo) FCFS(tasks []Task) {
 	// sort by start INT moment
 	sort.SliceStable(tasks, func(i, j int) bool {
-		return tasks[i].start < tasks[j].start;
+		return tasks[i].start < tasks[j].start
 	})
 
 	// iterate the processes
-	prs := make([]Process, len(tasks));
-	var lastEnd int = 0;
+	prs := make([]Process, len(tasks))
+	var lastEnd int = 0
 
-	var length int = tasks[2].start;
+	var length int = tasks[2].start
 
 	for i := range tasks {
-		length += tasks[i].time;
+		length += tasks[i].time
 	}
 
 	for i := 0; i < len(tasks); i++ {
-		prs[i].id = tasks[i].id;
-		var pEnd int = tasks[i].start + tasks[i].time;
-		var pStart int = tasks[i].start;
+		prs[i].id = tasks[i].id
+		var pEnd int = tasks[i].start + tasks[i].time
+		var pStart int = tasks[i].start
 
 		//iterate and fill processes
 
@@ -64,8 +66,8 @@ func (a *Algo) FCFS(tasks []Task) {
 				}
 			} else {
 				if j >= pStart && j <= lastEnd {
-					prs[i].appendToState(r);
-					pEnd++;
+					prs[i].appendToState(r)
+					pEnd++
 				} else if j > lastEnd && j <= pEnd {
 					prs[i].appendToState(p)
 				} else {
@@ -73,31 +75,39 @@ func (a *Algo) FCFS(tasks []Task) {
 				}
 			}
 		}
-		lastEnd = pEnd;
+		lastEnd = pEnd
 	}
 
-	fmt.Println(prs);
+	fmt.Println(prs)
 }
 
 func main() {
-	fmt.Println(n, r, p);
-	sol := &Algo{};
+	fmt.Println(n, r, p)
+	sol := &Algo{}
 
 	sol.FCFS([]Task{
 		{
-			id:1,
-			time:3,
-			start:1,
+			id:    1,
+			time:  3,
+			start: 1,
 		},
 		{
-			id:2,
-			time:4,
-			start:3,
+			id:    2,
+			time:  4,
+			start: 3,
 		},
 		{
-			id:3,
-			time:5,
-			start:6,
+			id:    3,
+			time:  5,
+			start: 6,
 		},
 	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World!")
+	})
+	err := http.ListenAndServe(":9000", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
